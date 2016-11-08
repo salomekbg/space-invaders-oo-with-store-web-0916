04a43f195a04b7cc6f75690f12fe6ed3eadc18e6
SluvsP <salomekbg@gmail.com>
2016-11-08 15:10:49 -0500

    Done.

---
+++ spaceship.js
@@ -0,0 +1,14 @@
+class Spaceship{
+
+  constructor(hash){
+    this.id = hash.id;
+    this.name = hash.name;
+    this.phasers = hash.phasers;
+    this.shields = hash.shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    this.docked = true;
+    Store.data.spaceships.push(this);
+  }
+}
